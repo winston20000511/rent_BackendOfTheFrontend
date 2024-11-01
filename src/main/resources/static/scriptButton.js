5195bf36dc83431c1c816ede8604103309bae447
Winston <winston20000511@gmail.com>
2024-11-01 23:24:03 +0800

    Create Project

---
+++ src/main/resources/static/scriptButton.js
@@ -0,0 +1,13 @@
+const filterBtn = document.getElementsByClassName("filter-btn");
+
+Array.from(filterBtn).forEach(btn => {
+    btn.addEventListener('click',()=>{
+        let i = btn.getElementsByClassName("fa-solid")[0];
+        if (i.className==="fa-solid fa-angle-down"){
+            i.className="fa-solid fa-angle-up";
+        }else{
+            i.className="fa-solid fa-angle-down";
+        }
+        
+    })
+});
