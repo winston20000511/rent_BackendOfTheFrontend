19b6908606dd1480ffd2f5b8b218819597780514
Strawberry <honeykokia@gmail.com>
2024-11-25 00:03:21 +0800

    add adress dto , but search need bebug

---
+++ src/main/java/com/example/demo/dto/AddressDTO.java
@@ -0,0 +1,36 @@
+package com.example.demo.dto;
+
+public class AddressDTO {
+
+	private String address;
+	private Double lat;
+	private Double lng;
+	private Integer price;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Double getLat() {
+		return lat;
+	}
+	public void setLat(Double lat) {
+		this.lat = lat;
+	}
+	public Double getLng() {
+		return lng;
+	}
+	public void setLng(Double lng) {
+		this.lng = lng;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	
+	
+}
