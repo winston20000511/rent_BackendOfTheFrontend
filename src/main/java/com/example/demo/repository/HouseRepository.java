2a5969de6b39b8ff0a47f6bed3dbab272101664b
winston20000511 <winston20000511@gmail.com>
2024-12-01 15:10:11 +0800

    2024/12/1 merge OK

--- src/main/java/com/example/demo/repository/HouseRepository.java
+++ src/main/java/com/example/demo/repository/HouseRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.dto.HouseDetailsDTO;
 import com.example.demo.model.HouseImageTableBean;
@@ -15,5 +16,10 @@ public interface HouseRepository extends JpaRepository<HouseTableBean, Long> {
 	
 	 List<HouseImageTableBean> findByHouseId(Long houseId);
 
-	List<Map<String, Object>> findNoAdHouses(Long userId);
+		
+		@Query(value="select h.house_id as houseId, h.title as houseTitle from house_table h "
+				+ "left join ads_table a on a.house_id = h.house_id "
+				+ "where h.user_id = :userId and a.ad_id is null",
+				nativeQuery = true)
+		public List<Map<String,Object>> findNoAdHouses(Long userId);
 }
