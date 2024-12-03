a3234935be526f52943721f11ae096cd4e99a317
winston20000511 <winston20000511@gmail.com>
2024-12-03 10:12:15 +0800

    2024/12/3 OK

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -4,21 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String home(Model model) {
-        return "layout/navbar"; // 渲染 src/main/resources/templates/layout/navbar.html
-    }
-    
-    @GetMapping("/login")
-    public String login(Model model) {
-        return "layout/login"; // src/main/resources/templates/layout/login.html
-    }
-    
-    @GetMapping("/forgot-password")
-    public String forgotPasswordPage(Model model) {
-        return "layout/forgot-password"; // 對應 src/main/resources/templates/layout/forgot-password.html
-    }
-}
+//@Controller
+//public class HomeController {
+//
+////    @GetMapping("/")
+////    public String home(Model model) {
+////        return "layout/navbar"; // 渲染 src/main/resources/templates/layout/navbar.html
+////    }
+////    
+////    @GetMapping("/login")
+////    public String login(Model model) {
+////        return "layout/login"; // src/main/resources/templates/layout/login.html
+////    }
+////    
+////    @GetMapping("/forgot-password")
+////    public String forgotPasswordPage(Model model) {
+////        return "layout/forgot-password"; // 對應 src/main/resources/templates/layout/forgot-password.html
+////    }
+////}
