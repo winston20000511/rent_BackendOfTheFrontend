d3acbd791ba1e2220fad92b0b3afafd43d586c68
Seiya-mui <vanessa0702128@gmail.com>
2024-12-11 01:35:23 +0800

    2024-12-11 update findNoAdHouses method
    update HouseRepository & AdService

--- src/main/java/com/example/demo/repository/HouseRepository.java
+++ src/main/java/com/example/demo/repository/HouseRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,14 +16,14 @@ import com.example.demo.model.HouseTableBean;
 public interface HouseRepository extends JpaRepository<HouseTableBean, Long> {
 
 	void save(HouseDetailsDTO house);
-		
-		@Query(value="select h.house_id as houseId, h.title as houseTitle from house_table h "
-				+ "left join ads_table a on a.house_id = h.house_id "
-				+ "where h.user_id = :userId and a.ad_id is null",
-				nativeQuery = true)
-		public List<Map<String,Object>> findNoAdHouses(Long userId);
-		
-		@Query("SELECT new com.example.demo.dto.HouseListByUserIdDTO(h.houseId, h.user.userId) " +
-			       "FROM HouseTableBean h WHERE h.user.userId = :userId")
-			List<HouseListByUserIdDTO> findHousesByUserId(@Param("userId") Long userId);
+
+	@Query(value = "select h.house_id as houseId, h.title as houseTitle from house_table h "
+			+ "left join ads_table a on a.house_id = h.house_id "
+			+ "where a.ad_id is null", countQuery = "select count(*) " + "from house_table h "
+					+ "left join ads_table a on a.house_id = h.house_id where a.ad_id is null", nativeQuery = true)
+	public Page<Map<String, Object>> findHousesWithoutAds(Pageable pageable);
+
+	@Query("SELECT new com.example.demo.dto.HouseListByUserIdDTO(h.houseId, h.user.userId) "
+			+ "FROM HouseTableBean h WHERE h.user.userId = :userId")
+	List<HouseListByUserIdDTO> findHousesByUserId(@Param("userId") Long userId);
 }
