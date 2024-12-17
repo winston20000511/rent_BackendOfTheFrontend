1b90bf1d72b9be6fceac967683227147f3d4b82d
winston20000511 <winston20000511@gmail.com>
2024-12-17 16:22:00 +0800

    2024/12/17 HOUSEPHOTO FIX

--- src/main/java/com/example/demo/repository/HouseImageRepository.java
+++ src/main/java/com/example/demo/repository/HouseImageRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.repository.query.Param;
 import com.example.demo.model.HouseImageTableBean;
 
 public interface HouseImageRepository extends JpaRepository<HouseImageTableBean, Long> {
-	@Query("SELECT h FROM HouseImageTableBean h WHERE h.house.id = :houseId")
-	List<HouseImageTableBean> findByHouseId(@Param("houseId") Long houseId);
+	@Query("SELECT h.images FROM HouseImageTableBean h WHERE h.house.id = :houseId")
+	List<byte[]> findImagesByHouseId(@Param("houseId") Long houseId);
 
 
 }
