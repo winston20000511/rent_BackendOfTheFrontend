ebec5f45e3dc0fb2de573b34e429ef28b47aa3e6
Strawberry <honeykokia@gmail.com>
2024-12-25 14:06:28 +0800

    merge debug and remove .properties , please install environment variable

--- src/main/java/com/example/demo/helper/EcpayApiConfig.java
+++ src/main/java/com/example/demo/helper/EcpayApiConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("ecpayapi.properties")
+//@PropertySource("ecpayapi.properties")
 public class EcpayApiConfig {
 	
 	@Value("${merchantid}")
