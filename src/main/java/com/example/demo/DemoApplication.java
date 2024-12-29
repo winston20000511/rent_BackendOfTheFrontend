81df97b7c12221eab65c060db1ead678bc8b6ab7
DESKTOP-RVHB2K0\Hoxton <123>
2024-12-29 15:18:06 +0800

    log env

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.example.demo.helper.SerialOrderNoUtil;
@@ -10,8 +13,10 @@ import com.example.demo.helper.SerialOrderNoUtil;
 
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class DemoApplication implements CommandLineRunner{
-
+	@Autowired
+	private Environment env;
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
@@ -19,7 +24,8 @@ public class DemoApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		SerialOrderNoUtil util = new SerialOrderNoUtil();
-
+		String dbUsername = env.getProperty("azure.db.username");
+		log.info("dbUsername"+dbUsername);
 		
 	}
 
